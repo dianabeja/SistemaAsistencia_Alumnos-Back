0346b904239d4f543e656aa6b3ee4c8892277293
Joseemmanuel <emmanuelriv15@gmail.com>
2023-11-07 15:51:41 -0600

    modified estudiantes controller

--- src/database/Estudiantes.js
+++ src/database/Estudiantes.js
@@ -1,21 +1,21 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'SIAEFINAL',
-  password: '24042002',
+  user: "postgres",
+  host: "localhost",
+  database: "siae",
+  password: "root",
   port: 5432,
-  connectionConfig:{
-    charset:'UTF8'
-  }
+  connectionConfig: {
+    charset: "UTF8",
+  },
 });
 
 // Obtener todos los estudiantes
 const getAllStudents = async () => {
   try {
-    const res = await pool.query('SELECT * FROM alumnos');
+    const res = await pool.query("SELECT * FROM alumnos");
     return res.rows;
   } catch (error) {
     throw error;
@@ -25,7 +25,10 @@ const getAllStudents = async () => {
 // Obtener un estudiante por ID
 const getOneStudent = async (id) => {
   try {
-    const res = await pool.query('select a.id,a.nombre,a.matricula,a.sexo,a.licenciatura,ca.url_imagen from alumnos a inner join cuenta_alumnos ca on a.matricula=ca.matricula where a.matricula=$1', [id]);
+    const res = await pool.query(
+      "select a.id,a.nombre,a.matricula,a.sexo,a.licenciatura,ca.url_imagen from alumnos a inner join cuenta_alumnos ca on a.matricula=ca.matricula where a.matricula=$1",
+      [id]
+    );
     return res.rows[0];
   } catch (error) {
     throw error;
@@ -37,10 +40,10 @@ const createStudent = async (studentData) => {
   try {
     const { nombre, matricula, sexo, licenciatura } = studentData;
     await pool.query(
-      'INSERT INTO alumnos (nombre, matricula, sexo, licenciatura) values ($1,$2,$3,$4)',
+      "INSERT INTO alumnos (nombre, matricula, sexo, licenciatura) values ($1,$2,$3,$4)",
       [nombre, matricula, sexo, licenciatura]
     );
-    return 'Estudiante creado exitosamente';
+    return "Estudiante creado exitosamente";
   } catch (error) {
     throw error;
   }
@@ -51,10 +54,10 @@ const updateStudent = async (id, studentData) => {
   try {
     const { nombre, matricula, sexo, licenciatura } = studentData;
     await pool.query(
-      'UPDATE alumnos SET nombre=$1, matricula=$2, sexo=$3, licenciatura=$4 WHERE id=$5',
+      "UPDATE alumnos SET nombre=$1, matricula=$2, sexo=$3, licenciatura=$4 WHERE id=$5",
       [nombre, matricula, sexo, licenciatura, id]
     );
-    return 'Estudiante actualizado exitosamente';
+    return "Estudiante actualizado exitosamente";
   } catch (error) {
     throw error;
   }
@@ -63,63 +66,77 @@ const updateStudent = async (id, studentData) => {
 // Eliminar un estudiante existente
 const deleteStudent = async (id) => {
   try {
-    await pool.query('DELETE FROM alumnos WHERE id = $1', [id]);
-    return 'Estudiante eliminado exitosamente';
+    await pool.query("DELETE FROM alumnos WHERE id = $1", [id]);
+    return "Estudiante eliminado exitosamente";
   } catch (error) {
     throw error;
   }
 };
 
-const verificarCuenta=async(cuenta)=>{
+const verificarCuenta = async (cuenta) => {
   try {
-    console.log(cuenta)
-    const {correo,contraseña}=cuenta;
-    console.log('correo'+correo)
-    console.log('contraseña'+contraseña)
+    console.log(cuenta);
+    const { correo, contraseña } = cuenta;
+    console.log("correo" + correo);
+    console.log("contraseña" + contraseña);
 
-    const res=await pool.query("SELECT matricula FROM cuenta_alumnos WHERE correo = $1 AND contraseña = $2 ",[correo,contraseña]);
-    console.log('Verificar cuenta', JSON.stringify(res));
+    const res = await pool.query(
+      "SELECT matricula FROM cuenta_alumnos WHERE correo = $1 AND contraseña = $2 ",
+      [correo, contraseña]
+    );
+    console.log("Verificar cuenta", JSON.stringify(res));
     return res.rows;
   } catch (error) {
     throw error;
   }
-}
+};
 
-const materias=async(id)=>{
+const materias = async (id) => {
   try {
     //const {correo,contraseña}=cuenta;
-    const res=await pool.query("select * from alumno_materias where matricula_alumno=$1; ",[id]);
+    const res = await pool.query(
+      "select * from alumno_materias where matricula_alumno=$1; ",
+      [id]
+    );
     return res.rows;
   } catch (error) {
     throw error;
   }
-}
+};
 
-const obtenerMaterias=async(id)=>{
+const obtenerMaterias = async (id) => {
   try {
     //const {correo,contraseña}=cuenta;
-    const res=await pool.query("select * from materias where nrc=$1; ",[id]);
+    const res = await pool.query("select * from materias where nrc=$1; ", [id]);
     return res.rows;
   } catch (error) {
     throw error;
   }
-}
+};
 
-const createCuenta=async(cuenta)=>{
+const createCuenta = async (cuenta) => {
   try {
-    const {matricula,correo,contraseña,url_imagen}=cuenta;
+    const { matricula, correo, contraseña, url_imagen } = cuenta;
 
-    const result = await pool.query('SELECT * FROM alumnos WHERE matricula = $1', [matricula]);
+    const result = await pool.query(
+      "SELECT * FROM alumnos WHERE matricula = $1",
+      [matricula]
+    );
     if (result.rows.length === 0) {
-      return 'No se encontró la matrícula';
+      return "No se encontró la matrícula";
     } else {
-      await pool.query('INSERT INTO cuenta_alumnos VALUES ($1, $2, $3, $4);', [matricula, correo, contraseña, url_imagen]);
-      return 'Cuenta creada exitosamente';
+      await pool.query("INSERT INTO cuenta_alumnos VALUES ($1, $2, $3, $4);", [
+        matricula,
+        correo,
+        contraseña,
+        url_imagen,
+      ]);
+      return "Cuenta creada exitosamente";
     }
   } catch (error) {
     throw error;
   }
-}
+};
 
 module.exports = {
   getAllStudents,
@@ -130,5 +147,5 @@ module.exports = {
   verificarCuenta,
   obtenerMaterias,
   materias,
-  createCuenta
+  createCuenta,
 };
